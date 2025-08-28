749f1d158789da00be6cdd8edf944dbf35decc2e
Kadukuntla Poornima <kadupoornima@google.com>
2025-08-28 05:38:14 +0000

    Revert gke-node-pool module to use google-beta provider

--- modules/compute/gke-node-pool/versions.tf
+++ modules/compute/gke-node-pool/versions.tf
@@ -18,7 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.41"
+      version = ">= 7.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 7.0"
     }
     null = {
       source  = "hashicorp/null"
